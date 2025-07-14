134762a962807930aa61d5b398ce5231a9587091
mathfis <matfis@gmail.com>
2025-07-14 14:09:31 -0300

    conclusão do estudo de 'tipos de operadores'

---
+++ tipos_de_operadores_com_python/operador_identidade.py
@@ -0,0 +1,10 @@
+saldo = 1000
+limite = 1000
+
+"""
+o operador 'is' indica se as variáveis 
+ocupam a mesma região de memória
+"""
+
+print(saldo is limite)
+print(saldo is not limite)
---
+++ tipos_de_operadores_com_python/operadores_de_associacao.py
@@ -0,0 +1,13 @@
+"""
+    operadores de associação indicam se
+    o elemento encontra-se em um iterável
+"""
+
+frutas = ['limão', 'uva']
+print('laranja' in frutas) # laranja está em frutas?
+print('limão' not in frutas) # limão não está em frutas?
+
+# Lembrete: Python é 'case sensitive'.
+curso = 'curso de python'
+print('python' in curso) #" 'python' está em curso?"
+print('Python' in curso) #" 'Python' está em curso? "
